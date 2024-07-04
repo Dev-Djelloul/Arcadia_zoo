3b4ac36b7fa01ef3494e60e3654ae09a1fedb9a5
Djelloul Abid <djelloulabid75@gmail.com>
2024-07-04 04:38:29 +0200

    "Réajustement de la structure du projet par arborescence de dossier/fichier"

--- zoo.sql
+++ zoo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : dim. 30 juin 2024 à 04:39
+-- Généré le : jeu. 04 juil. 2024 à 04:34
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -20,6 +20,20 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `zoo`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Animal`
+--
+
+CREATE TABLE `Animal` (
+  `id` int(11) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `race` varchar(255) NOT NULL,
+  `images` text DEFAULT NULL,
+  `habitat_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -43,7 +57,60 @@ INSERT INTO `avis` (`id`, `pseudo`, `avis`, `date`, `approuve`) VALUES
 (1, 'nicolas', 'super ludique ! ', '2024-06-30 01:05:41', 1),
 (2, 'djelloul', 'un parc plaisant et des animaux joyeux et jolis 😇🦅🦋🦕', '2024-06-30 01:19:08', 1),
 (3, 'elodie ', 'je me suis bien amusé en famille, les enfants ont adoré ! je reviendrais très prochainement :)', '2024-06-30 01:20:27', 1),
-(5, 'test', 'test\r\n', '2024-06-30 02:32:05', 0);
+(7, 'wilfried ', 'J\'ai passé ma journée au vivarium 🦎🐍🐊', '2024-06-30 03:26:18', 1),
+(8, 'batman', 'Les deux girafes se sont battues 🦒🤭', '2024-07-03 22:34:00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `CompteRenduVeterinaire`
+--
+
+CREATE TABLE `CompteRenduVeterinaire` (
+  `id` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `etat` varchar(255) NOT NULL,
+  `commentaire` text DEFAULT NULL,
+  `animal_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Consultation`
+--
+
+CREATE TABLE `Consultation` (
+  `id` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `alimentation` varchar(255) NOT NULL,
+  `grammage` double NOT NULL,
+  `animal_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Habitat`
+--
+
+CREATE TABLE `Habitat` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Service`
+--
+
+CREATE TABLE `Service` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -64,17 +131,49 @@ CREATE TABLE `Utilisateur` (
 INSERT INTO `Utilisateur` (`Username`, `MotDePasse`, `TypeUtilisateur`) VALUES
 ('admin@arcadiazoo.com', 'arcadia', 'administrateur'),
 ('employe@arcadiazoo.com', 'arcadia', 'employe'),
-('veterinaire@arcadiazoo.com', 'arcadia', 'veterinaire'),
-('visiteur@arcadiazoo.com', 'arcadia', 'visiteur');
+('veterinaire@arcadiazoo.com', 'arcadia', 'veterinaire');
 
 --
 -- Index pour les tables déchargées
 --
 
 --
+-- Index pour la table `Animal`
+--
+ALTER TABLE `Animal`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `habitat_id` (`habitat_id`);
+
+--
 -- Index pour la table `avis`
 --
 ALTER TABLE `avis`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `CompteRenduVeterinaire`
+--
+ALTER TABLE `CompteRenduVeterinaire`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `animal_id` (`animal_id`);
+
+--
+-- Index pour la table `Consultation`
+--
+ALTER TABLE `Consultation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `animal_id` (`animal_id`);
+
+--
+-- Index pour la table `Habitat`
+--
+ALTER TABLE `Habitat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `Service`
+--
+ALTER TABLE `Service`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -88,10 +187,62 @@ ALTER TABLE `Utilisateur`
 --
 
 --
+-- AUTO_INCREMENT pour la table `Animal`
+--
+ALTER TABLE `Animal`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT pour la table `avis`
 --
 ALTER TABLE `avis`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT pour la table `CompteRenduVeterinaire`
+--
+ALTER TABLE `CompteRenduVeterinaire`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `Consultation`
+--
+ALTER TABLE `Consultation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `Habitat`
+--
+ALTER TABLE `Habitat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `Service`
+--
+ALTER TABLE `Service`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `Animal`
+--
+ALTER TABLE `Animal`
+  ADD CONSTRAINT `animal_ibfk_1` FOREIGN KEY (`habitat_id`) REFERENCES `Habitat` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `CompteRenduVeterinaire`
+--
+ALTER TABLE `CompteRenduVeterinaire`
+  ADD CONSTRAINT `compterenduveterinaire_ibfk_1` FOREIGN KEY (`animal_id`) REFERENCES `Animal` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `Consultation`
+--
+ALTER TABLE `Consultation`
+  ADD CONSTRAINT `consultation_ibfk_1` FOREIGN KEY (`animal_id`) REFERENCES `Animal` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
