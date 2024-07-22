2ea9e18ffd7ea8e23b966535a45c38ca2afdae3d
Djelloul Abid <djelloulabid75@gmail.com>
2024-07-23 00:39:58 +0200

    "Mise à jour de la config MongoDb"

--- mongodb.php
+++ mongodb.php
@@ -1,7 +1,5 @@
- 
 <?php
 // Connexion à une base de données MongoDB et affichage des collections.
-
 
 // Utilisation de la bibliothèque MongoDB pour PHP.
 require_once 'vendor/autoload.php';
@@ -9,8 +7,9 @@ require_once 'vendor/autoload.php';
 use MongoDB\Client;
 
 try {
-    // Connexion à MongoDB
-    $client = new Client('mongodb://localhost:27017');
+    // Connexion à MongoDB via l'URI de la variable d'environnement
+    $mongoUri = getenv('MONGODB_URI'); // Obtention de l'URI depuis les variables d'environnement
+    $client = new Client($mongoUri);
     $db = $client->selectDatabase('zoo_db');
 
     echo "Connexion réussie à la base de données MongoDB!<br>";
