bc05372f7bf1f85b814add8d852b4b2fc761ee14
Djelloul Abid <djelloulabid75@gmail.com>
2024-07-22 05:09:47 +0200

    "test de connexion heroku"

--- test_db.php
+++ test_db.php
@@ -1,8 +1,11 @@
 <?php
 // Connexion MySQL
 $dbUrl = getenv('JAWSDB_URL');
-$dbParts = parse_url($dbUrl);
+if (!$dbUrl) {
+    die("L'URL de la base de données n'est pas définie dans les variables d'environnement.");
+}
 
+$dbParts = parse_url($dbUrl);
 $servername = $dbParts['host'];
 $username = $dbParts['user'];
 $password = $dbParts['pass'];
@@ -11,7 +14,6 @@ $dbname = ltrim($dbParts['path'], '/');
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
     echo "Connexion réussie !";
 
     // Exemple de requête pour vérifier la connexion
@@ -22,5 +24,16 @@ try {
 } catch (PDOException $e) {
     die("Erreur de connexion à la base de données: " . $e->getMessage());
 }
-?>
 
+// Connexion MongoDB
+require_once __DIR__ . '/vendor/autoload.php';
+
+function getMongoClient() {
+    $mongoUrl = getenv('MONGODB_URL');
+    if (!$mongoUrl) {
+        die("L'URL de MongoDB n'est pas définie dans les variables d'environnement.");
+    }
+    $client = new MongoDB\Client($mongoUrl);
+    return $client->zoo_db;
+}
+?>
