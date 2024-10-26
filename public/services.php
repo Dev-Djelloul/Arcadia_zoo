65aee71c7dae8aa06946875a684aa11963e5ea37
Djelloul Abid <djelloulabid75@gmail.com>
2024-10-26 02:17:44 +0200

    correction de la page Services

--- public/services.php
+++ public/services.php
@@ -1,7 +1,7 @@
 <?php
 require_once('../back-end-php/config.php');
 
-$sql = "SELECT NomService, DescriptionService, ImageService FROM services"; // Assurez-vous que le nom de la table et des colonnes est correct
+$sql = "SELECT NomService, DescriptionService, ImageService FROM Services"; // Assurez-vous que le nom de la table et des colonnes est correct
 $result = $conn->query($sql);
 
 // Debug : Affichage d'un message pour vérifier si la page se charge correctement
