36c21b35d13b8ffece152c9af602479e47c6c05f
Djelloul Abid <djelloulabid75@gmail.com>
2024-10-26 14:10:18 +0200

    encodage texte mail

--- back-end-php/formulaire_contact/send_email.php
+++ back-end-php/formulaire_contact/send_email.php
@@ -4,23 +4,29 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 use SendGrid\Mail\Mail;
 
 // Récupérer la clé API depuis les variables d’environnement
-$sendgridApiKey = getenv('SENDGRID_API_KEY'); // ou $_ENV['SENDGRID_API_KEY']
+$sendgridApiKey = getenv('SENDGRID_API_KEY') ?: $_ENV['SENDGRID_API_KEY'];
 
 // Récupération et sanitisation des données du formulaire
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 $subject = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
+// Échapper les caractères spéciaux pour éviter les problèmes d'encodage
+$email = htmlspecialchars($email, ENT_QUOTES, 'UTF-8');
+$subject = htmlspecialchars($subject, ENT_QUOTES, 'UTF-8');
+$description = htmlspecialchars($description, ENT_QUOTES, 'UTF-8');
+
 $mail = new Mail();
 $mail->setFrom('info.arcadiazoo@gmail.com', 'Contact client');
 $mail->setSubject($subject);
 $mail->addTo('info.arcadiazoo@gmail.com', 'Arcadia Zoo');
 
-$emailContent = "Vous avez reçu un nouveau message de la part d'un client.\n\n";
-$emailContent .= "Email: $email\n";
-$emailContent .= "Description:\n$description\n";
+$emailContent = "Vous avez reçu un nouveau message de la part d'un client.<br><br>";
+$emailContent .= "Email: $email<br>";
+$emailContent .= "Description:<br>$description<br>";
 
-$mail->addContent('text/plain', $emailContent);
+// Ajoute le contenu au format HTML
+$mail->addContent('text/html', $emailContent);
 
 $sendgrid = new \SendGrid($sendgridApiKey);
 
@@ -30,5 +36,4 @@ try {
 } catch (Exception $e) {
     echo 'Erreur lors de l\'envoi de l\'email: ' . $e->getMessage();
 }
-
 ?>
