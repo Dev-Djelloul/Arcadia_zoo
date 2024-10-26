bf5ce0ea1a097397de2ccf0c607a0ff9d1bbfd14
Djelloul Abid <djelloulabid75@gmail.com>
2024-10-26 08:30:36 +0200

    autoload

--- back-end-php/users/visiteur/update_consultation.php
+++ back-end-php/users/visiteur/update_consultation.php
@@ -1,6 +1,6 @@
 <?php
 // Connexion à une base de données MongoDB et mise à jour du compteur de consultations.
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../../vendor/autoload.php';
 
 use MongoDB\Client;
 
